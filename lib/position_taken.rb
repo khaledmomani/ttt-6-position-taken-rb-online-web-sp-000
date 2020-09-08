833c52b06a6fa95edfb9f2875eafb754474614ee
Khaled S Momani <khaledmomani4@gmail.com>
2020-09-08 01:13:09 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,3 +5,9 @@ def position_taken? (baord, index)
     return true
   end
 end
+
+def valid_move?(board, index)
+   if index.between?(0,8) && !position_taken?(board, index)
+      return true
+   end
+end
