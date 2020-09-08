c9f0364e808f6968b824d33107dcdb979a5e2a9b
Khaled S Momani <khaledmomani4@gmail.com>
2020-09-08 01:27:47 +0000

    Automatically backed up by Learn

---
+++ lib/position_taken.rb
@@ -0,0 +1,22 @@
+def valid_move?(board,index)
+  if  inxdex.to_i.between?(1,9) && position_taken?
+    true
+  else
+     false
+  end
+end
+
+
+
+def position_taken?(board,position)
+  if board[position] == " "
+    false
+    elsif board[position] == ""
+    false
+    elsif  board[position] == nil
+    false
+    else   board[position] == "X" || "O"
+    true
+
+  end
+end
