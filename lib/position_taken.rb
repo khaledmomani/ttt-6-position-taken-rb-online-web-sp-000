89a25dba8b8036dbe784554449a6b02f9cb33301
Khaled S Momani <khaledmomani4@gmail.com>
2020-09-08 01:09:08 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 def position_taken? (baord = [index])
-  if (board[index] == " ") || (board[index] == "")
+  if (board[index] == " ") || (board[index] == "") || (board[index] == "")
     return false
   end
 end
