5606ee28bd8b29469131bdfa0aac83d9dc46297f
Khaled S Momani <khaledmomani4@gmail.com>
2020-09-08 01:13:40 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,13 +1,8 @@
 def position_taken? (baord, index)
-  if (board[index] == " ") || (board[index] == "") || (board[index] == "" == nil) 
+  if (board[index] == " ") || (board[index] == "") || (board[index] == nil) 
     return false
   else
     return true
   end
 end
 
-def valid_move?(board, index)
-   if index.between?(0,8) && !position_taken?(board, index)
-      return true
-   end
-end
