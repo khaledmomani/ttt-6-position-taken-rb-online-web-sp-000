75d205cc1dc5ff8db39df79c1cb1ca9dd0495457
Khaled S Momani <khaledmomani4@gmail.com>
2020-09-08 01:17:14 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 require_family "./spec/position_taken_spec.rb"
 
 def position_taken? (baord, index)
-  if (board[index] == " ") || (board[index] == "") || (board[index] == nil) 
+  if (board [index] == " ") || (board [index] == "") || (board [index] == nil) 
     return false
   else
     return true
